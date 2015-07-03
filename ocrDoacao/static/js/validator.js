c5e77e1b7985049b3447d42d191133f2300a6639
rmmsilva <renato.massao@live.com>
2015-07-03 17:48:46 +0000

    validação da nota fiscal por javascript. Autor: Renato, Co-autora: Luciana.

--- ocrDoacao/static/js/validator.js
+++ ocrDoacao/static/js/validator.js
@@ -1,5 +1,9 @@
+var data_invalida = 0;
+var data_fora_prazo = 1;
+var data_valida = 2;
+    
 function validaCNPJ(cnpj) {
- 
+    var tamanho, numeros, digitos, soma, pos, resultado, i;
     cnpj = cnpj.replace(/[^\d]+/g,'');
  
     if(cnpj == '') return false;
@@ -63,22 +67,33 @@ function validaValor(valor) {
 }
 
 function validaData(data) {
-    var data = data.split('/');
-    var dia = parseInt(data[0], 10);
-    var mes = parseInt(data[1], 10);
-    var ano = parseInt(data[2], 10);
+    var data_split = data.split('/');
+    var dia = parseInt(data_split[0], 10);
+    var mes = parseInt(data_split[1], 10);
+    var ano = parseInt(data_split[2], 10);
     var novaData = new Date(ano,mes-1,dia);
     var hoje = new Date();
+    var primeiro_dia_valido;
 
     if (ano.toString().length != 4)
-        return false;
+        return data_invalida;
     if (novaData.getFullYear() != ano || novaData.getMonth() + 1 != mes || novaData.getDate() != dia) 
-        return false;
+        return data_invalida;
     if (novaData > hoje)
-        return false;
-    
-    return true;
-    
+        return data_invalida;
+    if (hoje.getDate() < 20) {
+        if (hoje.getMonth() > 0 ) {
+            primeiro_dia_valido = new Date(hoje.getFullYear(), hoje.getMonth() - 1, 1);
+        } else {
+            primeiro_dia_valido = new Date(hoje.getFullYear() - 1, 11, 1);
+        }
+    } else {
+        primeiro_dia_valido = new Date(hoje.getFullYear(), hoje.getMonth(), 1);
+    }
+    if (primeiro_dia_valido > novaData) {
+        return data_fora_prazo;
+    }
+    return data_valida;
 }
 
 function validaCOO(coo) {
@@ -88,3 +103,124 @@ function validaCOO(coo) {
         
     return true;
 }
+
+$(document).ready(function() {
+    var erro = false;
+    $("#data").mask("00/00/0000");
+    $("#valor").mask("###0.00", { reverse: true });
+    $("#coo").mask("000000");
+    $("#cnpj").mask("00.000.000/0000-00");
+    $("form").submit(function(e) {
+        var data = $("#data").val();
+        var valor = $("#valor").val();
+        var coo = $("#coo").val();
+        var cnpj = $("#cnpj").val();
+        if (data == null || data == "" ||
+            valor == null || valor == "" ||
+            coo == null ||  coo == "" ||
+            cnpj == null || cnpj == "")
+        {
+            if (!erro){
+                erro = true;
+                $("#erro").prepend("<p class='validation_error'>* Todos os campos são obrigatórios</p>");
+            }
+            e.preventDefault();
+        }
+    });
+    var msg_erros = {};
+    msg_erros['#cnpj'] = '* CNPJ inválido.';
+    msg_erros['#coo'] = '* COO inválido.';
+    msg_erros['#valor'] = '* Valor inválido.';
+    msg_erros['#data'] = [];
+    msg_erros['#data'][0] = '* Data inválida.';
+    msg_erros['#data'][1] = '* Data fora do prazo de doação';
+    
+    var funcoes_validacao = {};
+    funcoes_validacao['#cnpj'] = validaCNPJ;
+    funcoes_validacao['#coo'] = validaCOO;
+    funcoes_validacao['#data'] = validaData;
+    funcoes_validacao['#valor'] = validaValor;
+    
+    $.fn.extend({
+        pegaId: function () {
+            return '#' + $(this).attr('id');
+        },
+        mostraErro: function (msg) {
+            var id = $(this).pegaId();
+            if (typeof msg === 'undefined') {
+                msg = msg_erros[id];   
+            }
+            $(id).adicionaClasse('has-error');
+            $(id + ' + p').text(msg);
+       },
+       mostraSucesso: function () {
+            var id = $(this).pegaId(); 
+            $(id).adicionaClasse('has-success');
+            $(id + ' + p').text('');
+       },
+       adicionaClasse: function (classe) {
+           $(this).closest('.form-group').addClass(classe);
+       },
+       removeClasse: function (classe) {
+           $(this).closest('.form-group').removeClass(classe);
+       }
+    });
+    
+    $('form').submit(function (e) {
+        var cnpj, valor, coo, data;
+        var notaValida = true;
+        cnpj = $('#cnpj').val();
+        coo = $('#coo').val();
+        valor = $('#valor').val();
+        data = $('#data').val();
+        
+        $('.form-group p').text('');
+        $('.form-group').removeClass('has-error').removeClass('has-success');
+        if (!validaCNPJ(cnpj)) {
+            $('#cnpj').mostraErro();
+            notaValida = false;
+        } else {
+            $('#cnpj').mostraSucesso();
+        }
+        if (!validaCOO(coo)) {
+            $('#coo').mostraErro();
+            notaValida = false;
+        } else {
+            $('#coo').mostraSucesso();
+        }var data_validada = validaData(data);
+        
+        if (data_validada != data_valida) {
+            $('#data').mostraErro(msg_erros[data_validada]);
+            notaValida = false;
+        } else {
+            $('#data').mostraSucesso();
+        }
+        if (!validaValor(valor)) {
+            $('#valor').mostraErro();
+            notaValida = false;
+        } else {
+            $('#valor').mostraSucesso(); 
+        }
+        if (!notaValida) {
+            e.preventDefault();
+        }            
+    });
+    
+    $('input[type=text]').focusout(function () {
+        var valor_elemento = $(this).val();
+        var id = $(this).pegaId();
+        var funcao_validacao = funcoes_validacao[id];
+        var elemento_valido;
+        $(this).removeClasse('has-success');
+        $(this).removeClasse('has-error');
+        elemento_valido = funcao_validacao(valor_elemento);
+        if (typeof elemento_valido === 'number' && elemento_valido != data_valida) { // para datas
+            $(this).mostraErro(msg_erros['#data'][elemento_valido]);
+        } else if (!elemento_valido) {
+            $(this).mostraErro();
+        } else {
+            $(this).mostraSucesso();
+        }
+    });
+
+});
